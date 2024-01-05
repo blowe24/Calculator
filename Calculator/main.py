2207c281b5f5d4559f980198ec1213ed6ccc721b
Braden <109703250+blowe24@users.noreply.github.com>
2024-01-05 16:16:40 -0700

    Add files via upload

---
+++ Calculator/main.py
@@ -0,0 +1,27 @@
+def add(x, y):
+    return x + y
+
+def sub(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    return x, y
+
+def main():
+    operation = input("which opperatortion?\n(1. Add, 2. Subtract, 3. Multiply, 4. Divide)")
+    x = float(input("Enter the first number " ))
+    y = float(input('Enter the second number ' ))
+
+    if operation == '1':
+        print(x, '+' ,y, '=', add(x, y))
+    elif operation == '2':
+        print(x, '-' ,y, '=', sub(x, y))
+    elif operation == '3':
+        print(x, '*' ,y, '=', multiply(x, y))
+    elif operation == '1':
+        print(x, '/' ,y, '=', divide(x, y))    
+
+main()
